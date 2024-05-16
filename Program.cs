56f956b50a88a495dea92c5d7fabfa2fb0eea43c
Bruno <69668371+onurb143@users.noreply.github.com>
2024-05-16 03:08:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MomentozWebClient.Data;
+using MomentozWebClient.Controllers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+// Register FlightController as a transient service
+builder.Services.AddTransient<FlightController>();
+builder.Services.AddTransient<CustomerController>();
+
+var app = builder.Build();
+
+// Konfigurering af HTTP anmodnings pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. Yo   app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}");
+app.MapRazorPages();
+
+app.Run();
